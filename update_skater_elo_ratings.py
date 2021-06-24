d732819b29ab641b7fbaf6a00ce97ce644079733
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-06-23 23:27:01 -0400

    Update update_skater_elo_ratings.py

--- update_skater_elo_ratings.py
+++ update_skater_elo_ratings.py
@@ -73,8 +73,8 @@ def update_skater_elo_ratings_once(category='univariate-k_',data_source=random_r
 
     # Update elo skater_elo_ratings
     elo = skater_elo_update(elo=elo,k=k,data_source=data_source)
-    if False:
-        pprint(sorted(list(zip(elo['rating'],elo['name']))))
+    if True:
+        pprint(sorted(list(zip(elo['rating'],elo['name'])))[:5])
     print('',flush=True)
 
     # Try to save
